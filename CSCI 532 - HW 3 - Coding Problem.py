126f5d9e4a23818d8a238d762fe0a6f96a8c4ff9
bportil1 <135068971+bportil1@users.noreply.github.com>
2024-09-30 07:38:46 -0600

    Update CSCI 532 - HW 3 - Coding Problem.py

--- CSCI 532 - HW 3 - Coding Problem.py
+++ CSCI 532 - HW 3 - Coding Problem.py
@@ -2,92 +2,152 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 def generate_piece_wise_linear_data(lines: int, points: int) -> list[int]:
-
     idx = 0
     x = []
     y = []
     
-    
+    #Generate piecewise linear components
     for line in range(lines):
         x_temp = np.linspace(idx, (int(points/lines))+idx, int(points/lines))
-        y_temp = np.random.randint(-50,50) + x_temp + np.random.randint(-50,50) + np.random.normal(scale=1.0, size=int(points/lines))
+        y_temp = np.random.randint(-25,25) + x_temp + np.random.randint(-25,25) + np.random.normal(3, 2.5, size=int(points/lines))
         idx += (points/lines)
-
         x.extend(x_temp)
         y.extend(y_temp)
         
     return np.asarray(x), np.asarray(y)
 
-def calculate_best_fit_line(x_vals, y_vals, section_length):
-    if (section_length * sum(x_vals**2) - sum(x_vals)**2) == 0:
-        return 0, sum(y_vals) / section_length
-    m = ((section_length*sum(x_vals*y_vals)) - sum(x_vals)*sum(y_vals)) / (section_length * sum(x_vals**2) - sum(x_vals)**2)     
-    b = (sum(y_vals) - m*sum(x_vals)) / section_length
-    return m, b
+def generate_non_linear_data(points):
+    #Generate non-linear data
+    de_linearize = lambda X: np.cos(1.5 * np.pi * X) + np.cos( 5 * np.pi * X )
+    X = np.sort(np.random.rand(points)) * 2
+    y = de_linearize(X) + np.random.randn(points) * 0.1
 
-def calculate_errors(x_vals, y_vals):
+    return X, y
 
+
+def error_coefs(x, y):
+    n = len(x)
+    
+    if (n == 1):
+        return (0.0, y[0], 0.0)
+
+    #Compute terms for minimum error line calculation     
+    x_sum = np.sum(x)
+    y_sum = np.sum(y)
+    x_y_prod = np.sum(x * y)
+    x_sq_sum = np.sum(x ** 2)
+    a = (n*x_y_prod - x_sum*y_sum) / (n*x_sq_sum - x_sum*x_sum)
+    b = (y_sum - a*x_sum) / n
+
+    #Computer error for points around the minimum error line
+    error = np.sum((y-a * x-b)**2)
+    
+    return a, b, error
+
+def calculate_errors(x_vals, y_vals):  
     n = len(x_vals)
 
+    #Arrays for all errors and corresponding terms for backtracking
     errors = np.zeros((n,n))
+    a_vals = np.zeros((n,n))
+    b_vals = np.zeros((n,n))
+    
+    #Compute least squares error for segments between all pairs of points
     for j in range(n):
         for i in range(j+1):
-            section_length = j - i + 1
-            if section_length > 1:
-                m, b = calculate_best_fit_line(x_vals[i:i+section_length], y_vals[i:i+section_length], section_length)
-                errors[i, j] = sum((y_vals[i:i+section_length] - m*x_vals[i:i+section_length] - b)**2)
-    
-    return errors
+            a_vals[i, j], b_vals[i, j], errors[i, j] = error_coefs(x_vals[i:j+1], y_vals[i:j+1])
 
-def rebuild_opt_path(errors, length, c):
-    M = np.zeros(length)
-    p = np.zeros(length)
-    M_max = np.zeros(length)
-    p_max = np.zeros(length)
-    for j in range(1, length):
-        cost = [errors[i, j] + c + M[i-1] for i in range(j)]
-        M[j] = np.min(cost)
-        p[j] = np.argmin(cost)
-    return M, p
+    return a_vals, b_vals, errors
 
-def find_k_fitted_lines(M, p, k):
-    diffs = np.diff(M)
+def rebuild_opt_path(a_vals, b_vals, x, y, errors, C):
+    n = len(errors[0])
+    optimal_path = np.zeros(n)
+    #Sum errors for all segments 
+    for j in range(n):
+        temp_path = np.zeros(j+1)
+        temp_path[0] = errors[0,  j] + C
 
-    split_points = []
+        #Find optimal solution using the recursive definition
+        for i in range(1, j+1):
+            temp_path[i] = optimal_path[i-1] + errors[i, j] + C 
+        optimal_path[j] = np.min(temp_path)
 
-    if k <= 1:
-        return [0]
+    #Backtrack from end of dataset and rebuild optimal solution
+    optimal_coeffs = []
+    while j >= 0:
+        temp_opt_coeffs = np.zeros(j+1)
+        temp_opt_coeffs[0] = errors[0, j] + C
+        for i in range(1, j+1):
+            temp_opt_coeffs[i] = optimal_path[i-1] + errors[i, j] + C
+        idx_opt = np.argmin(temp_opt_coeffs)
+        a_optimal_section = a_vals[idx_opt,j]
+        b_optimal_section = b_vals[idx_opt,j]
+        if idx_opt <= 0:
+            x_min = float('-inf')
+        else:
+            x_min = (x[idx_opt-1] + x[idx_opt])/2
+        if j >= n-1:
+            x_max = float('inf')
+        else:
+            x_max = (x[j] + x[j+1])/2   
+        optimal_coeffs.insert(0, (x_min, x_max, a_optimal_section, b_optimal_section))
+        j = idx_opt - 1
+        
+    return optimal_coeffs
 
-    for idx in range(k):
-        split_points.append(np.argmax(diffs))
-        diffs[np.argmax(diffs)] = 0
+def fit_data(x, optimal_coeffs):
+    n = len(x)
+    y_approx = np.zeros(n)
+    #Approximate data using previously generated optimal path
+    for opt_coeff in optimal_coeffs:
+        ind = [i for i,elem in enumerate(x) if elem >= opt_coeff[0] and elem <= opt_coeff[1]]
+        y_approx[ind] = x[ind] * opt_coeff[2] + opt_coeff[3]
+    return y_approx
 
+def find_segments(x_vals, y_vals, max_segments, init_cost):
+    curr_C = init_cost
+    curr_num_segs = float('inf')
+    y_approx = 0
 
-    return sorted(split_points)
+    #Reapproximate data model by increasing error term until only the maximum
+    #number of segments are used
+    while curr_num_segs > max_segments:
+        curr_C += .05
+        a, b, errors = calculate_errors(x_vals, y_vals)
+        optimal_coeffs = rebuild_opt_path(a, b, x_vals, y_vals, errors, curr_C)
+        y_approx = fit_data(x_vals, optimal_coeffs)
+        curr_num_segs = len(optimal_coeffs)
+        
+    plot_approximation(x_vals, y_vals, y_approx, max_segments, curr_C)
+    return y_approx
 
-
-def plot_split_data(x_vals, y_vals, split_points):
-    plt.plot(x_vals, y_vals)
-    idx = 0
-    for point in split_points:
-        if len(x_vals[idx:point]) > 1:
-            m, b = calculate_best_fit_line(x_vals[idx:point], y_vals[idx:point], len(x_vals[idx:point]))
-            plt.plot(x_vals[idx:point], m*x_vals[idx:point]+b, linewidth=3, 
-                 label='line: ({:.2f}, {:.2f})'.format(m,b))
-        idx = point 
-
-    if len(x_vals[idx:]) > 1:
-        m, b = calculate_best_fit_line(x_vals[point:], y_vals[point:], len(x_vals[point:]))
-        plt.plot(x_vals[point:], m*x_vals[point:]+b, linewidth=3, 
-            label='line: ({:.2f}, {:.2f})'.format(m,b))
-    
+def plot_approximation(x_vals, y_vals, y_approx, max_segments, curr_C):
+    #Plot actual data alongside the approximation
+    fig = plt.figure()
+    title_label = "SLS\n" + "(" + str(max_segments) + " segments,  " + format(curr_C, ".2f") + " cost, " + str(len(x_vals)) + " points)\n"
+    fig.suptitle(title_label, fontsize=14, fontweight='bold')
+    fig.subplots_adjust(top=0.82)
+    plot_1 = fig.add_subplot(121)
+    plot_1.plot(x_vals, y_vals)
+    plot_1.set_title("Original Data", loc="left")
+    plot_2 = fig.add_subplot(122)
+    plot_2.plot(x_vals, y_approx, color="orange")
+    plot_2.set_title('SLS Approximation', loc="left")
     plt.show()
 
-c = 50
-x_vals, y_vals = generate_piece_wise_linear_data(15, 50)
-error = calculate_errors(x_vals, y_vals)
-M, p = rebuild_opt_path(error, len(x_vals), c)
-k = 10
-split_points = find_k_fitted_lines(M, p, k)
-plot_split_data(x_vals, y_vals, split_points)
+def test_driver():
+    #Generate piecewise linear components for simple, traceable examples
+    x_vals, y_vals = generate_piece_wise_linear_data(7, 40)
+    number_of_segments = 10
+    init_cost = .2
+    y_approx = find_segments(x_vals, y_vals, number_of_segments, init_cost)
+
+    #Generate non-linear data for more complicated examples
+    x_vals, y_vals = generate_non_linear_data(50)
+    number_of_segments = 10
+    init_cost = .2
+    y_approx = find_segments(x_vals, y_vals, number_of_segments, init_cost)
+
+if __name__ == '__main__':
+    test_driver()
 
